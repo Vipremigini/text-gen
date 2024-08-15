6c9898af484a909b16cacd1e051e03bf6e73a11c
Aditya P Venugopal <vipremigini@gmail.com>
2024-08-15 16:11:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from random import choice
 import math
 import re
-from nltk import ngrams
+
 
 f = open("text.txt", "r" )
 text = f.read()
 text = re.sub(r"[^a-zA-Z0-9\s]", "", text)
 text = text.lower()
-lst = ngrams(text.split(), 3)
-lst = list(n_grams)
+s = text.split()
+lst = []
+for i in range(len(s)-2):
+    k = [s[i],s[i+1],s[i+2]]
+    k = tuple(k)
+    lst.append(k)
+
 
 
 def gentmod(lst):
     mod = {}
-    for i in range(0,len(lst)):
+    print(lst[5])
+    for i in range(len(lst)):
+        
         f = lst[i][0:2]
-        n = lst[i][2]
+        n = lst[i]
         if f not in mod:
             mod[f] = {}
         if n not in mod[f]:
@@ -31,12 +38,14 @@ def getnt(mod, f):
             tokens.append(token)
     return choice(tokens)
 
-def gent(text,length):
-    model = gentmod(text)
-    currentt = choice(mod.keys())
+def gent(lst,length):
+    mod = gentmod(lst)
+    
+    currentt = choice(list(mod.keys()))
+    print(mod[currentt])
     output = currentt[0]+currentt[1]
     for i in range(0, length):
-        newt = getnt(model, currentt)
+        newt = getnt(mod, currentt)
         output += newt + " "
         currentt = newt
     print(output)
@@ -44,11 +53,4 @@ def gent(text,length):
 
 
 
-f = open("text.txt", "r" )
-text = f.read()
-text = re.sub(r"[^a-zA-Z0-9\s]", "", text)
-text = text.lower()
-print(text)
-gen(text,6,100)
-print('next')
-gent(text,200)
+gent(lst,200)
