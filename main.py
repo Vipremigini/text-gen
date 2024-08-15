2d95ee0ff9fc94b1fa5a06558ace8665cf1fa0e6
Aditya P Venugopal <vipremigini@gmail.com>
2024-08-15 15:38:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,31 @@
 from random import choice
 import math
 import re
+from nltk import ngrams
 
-def genmod(data, order):
+f = open("text.txt", "r" )
+text = f.read()
+text = re.sub(r"[^a-zA-Z0-9\s]", "", text)
+text = text.lower()
+lst = ngrams(text.split(), 3)
+lst = list(n_grams)
+
+
+def gentmod(lst):
     mod = {}
-    for i in range(0, len(data)-order):
-        f = data[i:i+order]
-        n = data[i+order]
+    for i in range(0,len(lst)):
+        f = lst[i][0:2]
+        n = lst[i][2]
         if f not in mod:
             mod[f] = {}
         if n not in mod[f]:
             mod[f][n] = 1
         else:
             mod[f][n] += 1
-    return mod
-
-
-def getn(mod, f):
-    letters = []
-    for letter in mod[f].keys():
-        for times in range(0, mod[f][letter]):
-            letters.append(letter)
-    return choice(letters)
-
-def gen(text, order, length):
-    model = genmod(text, order)
-    currentf = text[0:order]
-    output = ""
-    for i in range(0, length-order):
-        newc = getn(model, currentf)
-        output += newc
-        currentf = currentf[1:] + newc
-    print( output)
-
-
-def gentmod(data):
-    l = data.split()
-    mod = {}
-    for i in range(0,len(l)-1):
-        f = l[i]
-        n = l[i+1]
-        if f not in mod:
-            mod[f] = {}
-        if n not in mod[f]:
-            mod[f][n] = 1
-        else:
-            mod[f][n] += 1
-    #mod[l[-1]] = "/n"
     return mod
 
 def getnt(mod, f):
     tokens = []
-    print(mod[f])
     for token in mod[f].keys():
         for times in range(0, mod[f][token]):
             tokens.append(token)
@@ -59,8 +33,8 @@ def getnt(mod, f):
 
 def gent(text,length):
     model = gentmod(text)
-    currentt = 'the'
-    output = "the "
+    currentt = choice(mod.keys())
+    output = currentt[0]+currentt[1]
     for i in range(0, length):
         newt = getnt(model, currentt)
         output += newt + " "
