18fe4984431b51fa6bfc8bed1a0cd7e98ad584a6
Aditya P Venugopal <vipremigini@gmail.com>
2024-08-15 19:04:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,23 @@ headers = {
 }
 
 
+f = open("text.txt", "r" )
+text = f.read()
+text = re.sub(r"[^a-zA-Z0-9\s]", "", text)
+text = text.lower()
+s = text.split()
+lst = []
+for i in range(len(s)-2):
+    k = [s[i],s[i+1],s[i+2]]
+    k = tuple(k)
+    lst.append(k)
+
+mod = main.gentmod(lst)
 app = Flask(__name__)
 
 @app.post("/api/get")
 def get():
-    gen = main.gent(main.lst,100)
+    gen = main.gent(main.lst,100,mod)
     data = {
     'model': 'gpt-3.5-turbo',
     'messages': [
