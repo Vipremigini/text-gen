fe3cf1bcc611dd2964c0b25b5ab05693c42d41be
Aditya P Venugopal <vipremigini@gmail.com>
2024-08-15 18:18:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
 from flask import Flask, request
 import requests
-import time
+import os
+import main
 
 url = 'https://jamsapi.hackclub.dev/openai/chat/completions'
 
 headers = {
     'Content-Type': 'application/json',
-    'Authorization': ''
+    'Authorization': os.environ.get("aikey")
 }
-data = {
-    'model': 'gpt-3.5-turbo',
-    'messages': [
-        {
-            'role': 'user',
-            'content': ''
-        }
-    ]
-}
+
 
 app = Flask(__name__)
 
 @app.post("/api/get")
 def get():
+    gen = main.gent(main.lst,100)
+    data = {
+    'model': 'gpt-3.5-turbo',
+    'messages': [
+        {
+            'role': 'user',
+            'content': " Explain this philosohy paragraph. return in one senstence. Reply like givng an advice: " + str(gen)}]}
+
     response = requests.post(url, headers=headers, json=data)
     rdata = response.json()
     rtext = rdata['choices'][0]['message']['content']
